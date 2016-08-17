f7e86ca2fa38a83bd559ede6b99979f942360f61
Tim3tang <Tim3tang@gmail.com>
2016-08-17 16:12:13 +0100

    11 instance variables changes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,9 +1,17 @@
 require_relative "bike"
 
+
+
 class DockingStation
 
   def release_bike
     @bike = Bike.new
   end
+
+  def dock(bike)
+    @bike = bike
+  end
+
+attr_reader :bike
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,18 +3,29 @@ require 'docking_station'
 docking_station = DockingStation.new
 bike = docking_station.release_bike
 
-describe "docking station" do
+describe DockingStation do
 
   it "when asking DockingStation to release_bike, a bike is released" do
     expect(docking_station).to respond_to(:release_bike)
   end
 
-  it "When asking if bike is working, passes true." do
-    expect(docking_station.working(bike)).to eq true
-  end
-
   it "released working bikes" do
     bike = docking_station.release_bike
     expect(bike).to be_working
+  end
+
+  it "docks bike at docking station" do
+    expect(docking_station).to respond_to(:bike)
+    end
+
+  it "docks something" do
+    bike = Bike.new
+    expect(subject.dock(bike)).to eq bike
+  end
+
+  it "returns docked bikes" do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.bike).to eq bike
   end
 end
